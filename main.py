dacd98220fceae99af575243f7037ad4f014a5f8
Aura <aura01221@gmail.com>
2023-09-26 09:43:35 +0800

    20230926

--- main.py
+++ main.py
@@ -8,11 +8,9 @@ result = subprocess.run(['net', 'start', 'chromoting'], shell=True)
 # print(result.returncode)
 
 if result.returncode==2:
-    # os.system("net stop chromoting >/dev/null 2>&1")
     subprocess.Popen('net stop chromoting', shell=True)
     newToast.text_fields = ['Stop Chrome Remote']
 else:
-    # os.system("net start chromoting >/dev/null 2>&1")
     subprocess.Popen('net start chromoting', shell=True)
     newToast.text_fields = ['Start Chrome Remote']
 
